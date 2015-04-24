1f484e0b68a0fa5ce0d54c22c731ea230d14c181
Jorge Hernandez <jorgeh905187@gmail.com>
2015-04-24 11:18:42 -0700

    new repo

---
+++ php/controller/login-verify.php
@@ -0,0 +1,19 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+    
+    function authenticateUser() {
+        if(!isset($_SESSION["authenticated"])) {
+            
+        }
+        else {
+            if($_SESSION["authenticated"] != true){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+    }
+    
+    
+
